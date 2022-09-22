d1ebb8179a37570c3285715a952aec8db54842ce
gu-geonhoe <rjsghl92@gmail.com>
2022-09-22 23:30:37 +0900

    2

--- src/main/java/com/example/testt/TesttApplication.java
+++ src/main/java/com/example/testt/TesttApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TesttApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TesttApplication.class, args);
     }
