89d44b0bab7c314d20685c419a9bd1107f2b9f21
gu-geonhoe <rjsghl92@gmail.com>
2022-09-24 16:50:01 +0900

    c

--- src/main/java/com/example/testt/Controller.java
+++ src/main/java/com/example/testt/Controller.java
@@ -1,17 +1,25 @@
 package com.example.testt;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
+@RequiredArgsConstructor
 public class Controller {
 
+    private final MemberRepository memberRepository;
     @GetMapping
     public String hellow(){
         return "hellow332333";
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity test(@RequestBody Member member){
+        memberRepository.save(member);
+return new ResponseEntity<>("등록완료", HttpStatus.OK);
     }
 
 }
