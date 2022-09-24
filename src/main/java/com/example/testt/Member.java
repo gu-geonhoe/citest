234b17cad8c31a26fcbc92d7e019d831acfcfe74
gu-geonhoe <rjsghl92@gmail.com>
2022-09-24 18:21:22 +0900

    x

--- src/main/java/com/example/testt/Member.java
+++ src/main/java/com/example/testt/Member.java
@@ -2,6 +2,8 @@ package com.example.testt;
 
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@Getter
+@Setter
 public class Member {
 
     @Id
